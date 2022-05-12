af9a73702d0a392e506585e0d5cf61c7ba6220f9
adrianrusu26 <adrian.rusu176@gmail.com>
2022-05-12 21:51:16 +0300

    thread affair tiles 2

--- components/main.jsx
+++ components/main.jsx
@@ -55,7 +55,120 @@ export default function Main() {
             </p>
           </header>
 
+          <section className="product-tiles row">
+            <div className="col-12 col-lg-3">
+              <ProductTile></ProductTile>
+            </div>
+            <div className="col-12 col-lg-3 offset-lg-1 mt-4 mt-lg-0">
+              <ProductTile></ProductTile>
+            </div>
+            <div className="col-12 col-lg-3 offset-lg-1 mt-4 mt-lg-0">
+              <ProductTile></ProductTile>
+            </div>
+          </section>
+
+          <footer>
+            <a href="" title="See more" className="catalog-preview-cta">
+              see more
+            </a>
+          </footer>
+        </section>
+
+        <section className="home-catalog-preview container demo-flex">
+          <header>
+            <h1 className="section-title">
+              <span>Layout</span> Flexbox
+            </h1>
+
+            <p>
+              Accomplish new achievements with a bold new style from the
+              brilliant collection of men’s designer suits.
+            </p>
+
+            <p>
+              For the finest custom tailored experience, be sure to explore our
+              selection of premium fabrics from one of the world's oldest fabric
+              mills, Vitale Barberis Canonico.
+            </p>
+          </header>
+
           <section className="product-tiles">
+            <ProductTile></ProductTile>
+
+            <ProductTile></ProductTile>
+
+            <ProductTile></ProductTile>
+          </section>
+
+          <footer>
+            <a href="" title="See more" className="catalog-preview-cta">
+              see more
+            </a>
+          </footer>
+        </section>
+
+        <section className="home-catalog-preview container ">
+          <header>
+            <h1 className="section-title">
+              <span>Layout</span> Bootstrap flex
+            </h1>
+
+            <p>
+              Accomplish new achievements with a bold new style from the
+              brilliant collection of men’s designer suits.
+            </p>
+
+            <p>
+              For the finest custom tailored experience, be sure to explore our
+              selection of premium fabrics from one of the world's oldest fabric
+              mills, Vitale Barberis Canonico.
+            </p>
+          </header>
+
+          <section className="product-tiles d-flex flex-column flex-lg-row justify-content-between">
+            <div className="mb-5 mg-lg-0">
+              <ProductTile></ProductTile>
+            </div>
+
+            <div className="mb-5 mb-lg-0">
+              <ProductTile></ProductTile>
+            </div>
+
+            <div>
+              <ProductTile></ProductTile>
+            </div>
+          </section>
+
+          <footer>
+            <a href="" title="See more" className="catalog-preview-cta">
+              see more
+            </a>
+          </footer>
+        </section>
+
+        <section className="home-catalog-preview container demo-grid">
+          <header>
+            <h1 className="section-title">
+              <span>CSS</span> Grid layout
+            </h1>
+
+            <p>
+              Accomplish new achievements with a bold new style from the
+              brilliant collection of men’s designer suits.
+            </p>
+
+            <p>
+              For the finest custom tailored experience, be sure to explore our
+              selection of premium fabrics from one of the world's oldest fabric
+              mills, Vitale Barberis Canonico.
+            </p>
+          </header>
+
+          <section className="product-tiles">
+            <ProductTile></ProductTile>
+
+            <ProductTile></ProductTile>
+
             <ProductTile></ProductTile>
           </section>
 
